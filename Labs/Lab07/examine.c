7efaae7533e3863bea0af02a877735b3910b1670
Gordon A Rose <gar018@linuxremote1.bucknell.edu>
2023-02-25 20:14:26 -0500

    lab07 prelab complete

--- Labs/Lab07/examine.c
+++ Labs/Lab07/examine.c
@@ -1,9 +1,10 @@
 /*
  * A Linux file examiner
  * CSCI206
- * <Your Name>
+ * Gordon Rose
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
@@ -24,6 +25,25 @@ int main(int argc, char *argv[]) {
 	}
 
 	// use fstat to examine file status
+    struct stat * thisFile = malloc(sizeof(struct stat));
+    fstat(f,thisFile);
+    printf("st_dev: %ld\n",thisFile->st_dev);
+    printf("st_ino: %ld\n",thisFile->st_ino);
+    printf("st_mode: %o\n",thisFile->st_mode);
+    printf("st_nlink: %ld\n",thisFile->st_nlink);
+    printf("st_uid: %ud\n",thisFile->st_uid);
+    printf("st_gid: %ud\n",thisFile->st_gid);
+    printf("st_rdev: %ld\n",thisFile->st_rdev);
+    printf("st_size: %ld\n",thisFile->st_size);
+    printf("st_blksize: %ld\n",thisFile->st_blksize);
+    printf("st_blocks: %ld\n",thisFile->st_blocks);
+
+    //const time_t * atime = &thisFile->st_atime;
+    printf("st_atime: %s", ctime(&thisFile->st_atime));
+    printf("st_mtime: %s",ctime(&thisFile->st_mtime));
+    printf("st_ctime: %s",ctime(&thisFile->st_ctime));
+    
+    free(thisFile);
 
 	close (f);
 	return 0;
